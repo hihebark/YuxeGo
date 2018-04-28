c4317182066c20783a7334e66e271632e380744d
hihebark <hihebark@gmail.com>
2018-04-28 17:41:58 +0100

    fixed the convertion to mp3 with ffmpeg

--- lib/util.go
+++ lib/util.go
@@ -10,11 +10,12 @@ import (
 // return cmd (output) or emty string if error.
 func Execute(pathExec string, args []string) (string, error) {
 
+	fmt.Printf("%v\n", args)
 	path, err := exec.LookPath(pathExec)
 	if err != nil {
 		return "", err
 	}
-	cmd, err := exec.Command(path, args...).Output()
+	cmd, err := exec.Command(path, args...).CombinedOutput()
 	if err != nil {
 		return string(cmd), err
 	}
@@ -26,15 +27,16 @@ func ConvertToMp3(video string) {
 	if existe(video + ".flv") {
 		args := []string{
 			"-c",
-			"ffmpeg",
-			"-i",
-			fmt.Sprintf("%s.flv", video),
-			"-b:a 192K",
-			"-vn",
-			fmt.Sprintf("%s.mp3", video),
+			//"ffmpeg",
+			//"-i",
+			//fmt.Sprintf("%s.flv", video),
+			//"-b:a 192K",
+			//"-vn",
+			fmt.Sprintf("ffmpeg -i '%s.flv' -b:a 192K -vn '%s.mp3'", video, video),
 		}
-		_, err := Execute("/bin/sh", args)
+		out, err := Execute("/bin/sh", args)
 		Printerr(err, "utils:convertToMp3:Execute "+video)
+		fmt.Printf("%v\n", out)
 	} else {
 		Bad("No such a file")
 	}
